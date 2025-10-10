5f792175f37913a63b4f2a9f125312c89ee52291
Flint Mueller <flintthecuber@gmail.com>
2025-10-10 11:49:44 -0400

    back to ctrl f

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -40,7 +40,7 @@ local config = {
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<C-I>', function()
+        vim.keymap.set('n', '<C-f>', function()
             vim.lsp.buf.format { async = true }
         end, opts)
     end,
