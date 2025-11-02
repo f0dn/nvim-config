cb1b5c4061c8ddfd482d3ec1fb9e1999cbf3daae
Flint Mueller <flintthecuber@gmail.com>
2025-11-02 16:48:21 -0500

    telescope fixes + live_grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,8 @@
-require('telescope').setup {
+local telescope = require('telescope')
+local telescope_builtin = require('telescope.builtin')
+
+telescope.setup {
     defaults = {
-        hidden = true,
         file_ignore_patterns = { '%.class', '%.bin', '%.dex', '%.flat', '%.lock', '%.jar', '.git/' },
         mappings = {
             i = {
@@ -8,13 +10,11 @@ require('telescope').setup {
                 ["<C-k>"] = "move_selection_previous",
             }
         },
-        pickers = {
-            find_files = {
-                hidden = true,
-            },
-        },
     }
 }
 
-vim.api.nvim_set_keymap('n', '<leader>pf', ':lua require("telescope.builtin").find_files({ hidden = true })<CR>',
+vim.keymap.set('n', '<leader>pf', function() telescope_builtin.find_files({ hidden = true }) end,
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ps',
+    function() telescope_builtin.live_grep({ additional_args = function() return { '--hidden' } end }) end,
     { noremap = true, silent = true })
