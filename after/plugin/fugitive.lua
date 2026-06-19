2af548ea1036f5ad8720e383581064198b27f298
Flint Mueller <flintthecuber@gmail.com>
2026-06-19 11:16:52 -0400

    autodetect dotfiles

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,16 @@
+local old_git = vim.cmd.Git
+
+local function git()
+    if vim.fn.FugitiveGitDir() == '' then
+        local git_dir = vim.fn.expand('~/.dotfiles')
+        local work_tree = vim.fn.expand('~')
+
+        vim.fn.FugitiveDetect(git_dir, work_tree)
+    end
+
+    old_git()
+end
+
+vim.cmd.Git = git
+
 vim.keymap.set('n', '<leader>g', vim.cmd.Git, { desc = 'Open Git status' })
